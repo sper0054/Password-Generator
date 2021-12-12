1e4e4d748aa4fcb4e0399fc2bd83f1079a518860
sper0054 <teresa.sperl@gmail.com>
2021-12-12 14:33:26 -0600

    prompts working

--- script.js
+++ script.js
@@ -1,14 +1,56 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// Add event listener to generate button
+document.querySelector("#generate").addEventListener("click", writePassword);
 
-// Write password to the #password input
-function writePassword() {
+
+//variables & arrays
+var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var special= [ "!", "@", "#", "$", "%", "^", "&", "*", "~", "{", "}", "|","_","[", "]","?", "`", "(", ")", "+", "-", ".", "/", "<", ">", "="];
+
+
+//generate password function
+function generatePassword() {
+    //length prompt
+    var lengthprompt = window.prompt('How many characters, between 8 and 128, would you like your password to contain?');
+      //check length parameters to be between 8 and 28 and not blank. if unacceptable return to the prompt.
+      if ( lengthprompt === "" || lengthprompt === null || lengthprompt < 8 || lengthprompt > 128) {
+        window.alert('Must be a valid length between 8 and 128. Please try again.');
+       return generatePassword();
+    }
+    //log the prompt results and alert
+    console.log (lengthprompt)
+    window.alert ("Your password will be " + lengthprompt + " characters long.")
+
+    //ask player if they'd like lowercase
+    var asklower = window.confirm('Click OK to include lowercase characters.');
+  
+    //ask player if they'd like uppercase
+    var askupper = window.confirm('Click OK to include uppercase characters.');
+
+    //ask player if they'd like numbers
+    var asknumeric = window.confirm('Click OK to include numeric characters.');
+
+    //ask player is they'd like special characters
+    var askspecial = window.confirm('Click OK to include special characters');
+
+    //ensure one character type (lower/upper/numbers/special) is true
+    while (asklower === false && askupper === false && asknumeric === false && askspecial === false) {
+    window.alert ('At least one character type must be selected');
+      var asklower = window.confirm('Click OK to include lowercase characters.');
+      var askupper = window.confirm('Click OK to include uppercase characters.');
+      var asknumeric = window.confirm('Click OK to include numeric characters.');
+      var askspecial = window.confirm('Click OK to include special characters');
+    }
+
+    //for (var i = 0; i < )
+  };
+
+// Write password function
+function writePassword () {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+};
 
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
